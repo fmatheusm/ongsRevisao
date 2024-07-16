7661df58fa9d01e0fa940ff301843eebfb3b6f2d
Matheus Fernandes Martins <matheus.fmartins@bancobmg.com.br>
2024-07-16 19:30:50 -0300

    first commit

---
+++ cypress/e2e/ongs.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Ongs', () => {
+    it.skip('devem poder realizer um cadastro', () => {
+        cy.visit('localhost:3000/register');
+        cy.get('[data-cy=name]').type('Dogs queridos');
+        cy.get('[data-cy=email]').type('dog@mail.com');
+        cy.get('[data-cy=whatsapp]').type('31993344565');
+        cy.get('[data-cy=city]').type('Belo Horizonte');
+        cy.get('[data-cy=uf]').type('MG');
+
+        cy.intercept('POST', '**/ongs').as('postOng');
+
+        cy.get('[data-cy=submit]').click();
+
+        cy.wait('@postOng').then(xhr => {
+            expect(xhr.response.statusCode).be.eq(200);
+            expect(xhr.response.body).has.property('id');
+            expect(xhr.response.body.id).is.not.null;
+        });
+    });
+
+    it('deve poder realizar um login no sistema', () => {
+        cy.visit('localhost:3000');
+        cy.get('input[data-cy=id]').type(Cypress.env('createdOngId'));
+        cy.get('button[type=submit]').click();
+    });
+});
